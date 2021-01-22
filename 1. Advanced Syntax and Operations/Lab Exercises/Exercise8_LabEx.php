79dd17b6912d6ccd902ef89311f4e7f7cdd65780
p4nd4ta <stu1901681077@uni-plovdiv.bg>
2021-01-22 02:58:24 +0200

    add ex 8

--- 1. Advanced Syntax and Operations/Lab Exercises/Exercise8_LabEx.php
+++ 1. Advanced Syntax and Operations/Lab Exercises/Exercise8_LabEx.php
@@ -1,1 +1,20 @@
 <?php
+
+$numbers = explode(" ",readline());
+$count = intval(readline());
+
+$result = array_fill(0,count($numbers),0);
+
+for ($i=0; $i<$count; $i++){
+
+    $lastElement = array_pop($numbers);
+    array_unshift($numbers, $lastElement);
+
+    for ($j = 0; $j < count($numbers); $j++){
+        $result[$j] += $numbers[$j];
+    }
+}
+
+foreach ($result as $item){
+    echo $item . " ";
+}
